e9d0c451da5568fa618d8406a0c5369fb20048d3
Sehyo Chang <sehyo@fluvio.io>
2024-05-21 17:16:36 -0700

    add test

--- crates/sdf-http-test/src/lib.rs
+++ crates/sdf-http-test/src/lib.rs
@@ -1,1 +1,19 @@
 
+mod bindings {
+    wit_bindgen::generate!({
+        path: "wit",
+        world: "test-world",
+    });
+}
+
+bindings::export!(TestComponent with_types_in bindings);
+
+use bindings::exports::sdf::test::test_guest::Guest as TestGuest;
+
+struct  TestComponent;
+
+impl TestGuest for TestComponent {
+    fn run() -> String {
+        todo!()
+    }
+}

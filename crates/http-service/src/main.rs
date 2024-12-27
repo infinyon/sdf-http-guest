1fbce8cdb8c55eef96c0fc84154395483ef2b565
Sehyo Chang <sehyo@fluvio.io>
2024-12-26 21:43:23 -0800

    update dep add hello world service add test dataflow add test dataflow and run in ci

---
+++ crates/http-service/src/main.rs
@@ -0,0 +1,22 @@
+use poem::{
+    get, handler, listener::TcpListener, middleware::Tracing, web::Path, EndpointExt, Route, Server,
+};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello-{name}")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "poem=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let app = Route::new().at("/hello/:name", get(hello)).with(Tracing);
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .name("hello-world")
+        .run(app)
+        .await
+}

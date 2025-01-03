b227db581d0cc87ee8f5074c8e0e0575955ffe75
Sehz <sehyo@infinyon.com>
2025-01-03 07:53:47 -0800

    add new body based wrapper.  it is optimize for post (#15)
    update README

--- crates/sdf-http/src/lib.rs
+++ crates/sdf-http/src/lib.rs
@@ -14,13 +14,24 @@ mod bindings {
     });
 }
 
+/// re-export serde
+#[cfg(feature = "serde")]
+pub use serde;
+
+/// re-export serde_json
+#[cfg(feature = "serde_json")]
+pub use serde_json;
+
 pub use wrapper::*;
 
 // high level API similar to Reqwest
 mod wrapper {
-    use std::ops::{Deref, DerefMut};
+    use std::{
+        any::Any,
+        ops::{Deref, DerefMut},
+    };
 
-    use http::{Error, Response, Uri};
+    use http::{request::Builder, Error, HeaderName, HeaderValue, Response, Uri, Version};
 
     pub struct ByteResponse(Response<Vec<u8>>);
 
@@ -69,9 +80,133 @@ mod wrapper {
         T: TryInto<Uri>,
         <T as TryInto<Uri>>::Error: Into<Error>,
     {
-        let request = crate::http::Request::builder().uri(uri).body("")?;
-        let response = crate::blocking::send(request)?;
-        Ok(response.into())
+        let body_helper = BodyBuilder::empty();
+        body_helper.get(uri)
+    }
+
+    /// JSON request builder helper
+    pub fn json<T>(body: T) -> BodyBuilder<T> {
+        let request = crate::http::Request::builder().header("Content-Type", "application/json");
+        BodyBuilder {
+            body,
+            builder: request,
+        }
+    }
+
+    /// Body based builder, we subvert the Request builder oriented toward body
+    /// This make it easier to build request with body
+    pub struct BodyBuilder<Body> {
+        body: Body,
+        builder: Builder,
+    }
+
+    impl BodyBuilder<Vec<u8>> {
+        pub fn empty() -> Self {
+            Self {
+                body: Vec::new(),
+                builder: crate::http::Request::builder(),
+            }
+        }
+    }
+
+    impl<Body> BodyBuilder<Body>
+    where
+        Body: AsRef<[u8]>,
+    {
+        pub fn body(self) -> Body {
+            self.body
+        }
+
+        pub fn body_ref(&self) -> &Body {
+            &self.body
+        }
+
+        pub fn version(self, version: Version) -> Self {
+            Self {
+                builder: self.builder.version(version),
+                body: self.body,
+            }
+        }
+
+        /// set authorization header
+        pub fn auth<V>(self, token: V) -> Self
+        where
+            V: TryInto<HeaderValue>,
+            <V as TryInto<HeaderValue>>::Error: Into<Error>,
+        {
+            self.header("Authorization", token)
+        }
+
+        /// set bearer token, this is a shortcut for `Authorization: Bearer
+        pub fn bearer(self, token: &str) -> Self {
+            self.auth(format!("Bearer {}", token))
+        }
+
+        pub fn header<K, V>(self, key: K, value: V) -> Self
+        where
+            K: TryInto<HeaderName>,
+            <K as TryInto<HeaderName>>::Error: Into<Error>,
+            V: TryInto<HeaderValue>,
+            <V as TryInto<HeaderValue>>::Error: Into<Error>,
+        {
+            Self {
+                builder: self.builder.header(key, value),
+                body: self.body,
+            }
+        }
+
+        pub fn extension<T>(self, extension: T) -> Self
+        where
+            T: Clone + Any + Send + Sync + 'static,
+        {
+            Self {
+                builder: self.builder.extension(extension),
+                body: self.body,
+            }
+        }
+
+        /// invoke post call with the given URI and return the response
+        pub fn post<T>(self, uri: T) -> anyhow::Result<ByteResponse>
+        where
+            T: TryInto<Uri>,
+            <T as TryInto<Uri>>::Error: Into<Error>,
+        {
+            let request = self
+                .builder
+                .uri(uri)
+                .method(http::Method::POST)
+                .body(self.body)?;
+            let response = crate::blocking::send(request)?;
+            Ok(response.into())
+        }
+
+        pub fn get<T>(self, uri: T) -> anyhow::Result<ByteResponse>
+        where
+            T: TryInto<Uri>,
+            <T as TryInto<Uri>>::Error: Into<Error>,
+        {
+            let request = self
+                .builder
+                .uri(uri)
+                .method(http::Method::GET)
+                .body(self.body)?;
+            let response = crate::blocking::send(request)?;
+            Ok(response.into())
+        }
+    }
+
+    impl<Body> Deref for BodyBuilder<Body> {
+        type Target = Builder;
+
+        fn deref(&self) -> &Self::Target {
+            &self.builder
+        }
+    }
+
+    impl<Body> DerefMut for BodyBuilder<Body> {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.builder
+        }
     }
 }
 
